8747fddf910d39736ef87510d58c4c5a86bfc7fe
Louis Lui <louislui@Louiss-MacBook-Pro.local>
2019-05-07 11:37:53 +0800

    Problem: Header icons needs to be changed
    Solution: Change the icons

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-import AuthStackNavigator from './AuthStackNavigator';
-import MainDrawerNavigator from './MainDrawerNavigator';
+
+import MainStackNavigator from './MainStackNavigator';
 
 export default createAppContainer(createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-//  Auth: AuthStackNavigator,
-  Main: MainDrawerNavigator,
+  Main: MainStackNavigator,
 }));
---
+++ src/navigation/MainStackNavigator.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator , createDrawerNavigator} from 'react-navigation';
+
+import HomeScreen from '../screens/HomeScreen';
+import LinksScreen from '../screens/LinksScreen';
+import { Icon } from 'native-base';
+const HomeStack = createStackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+  },
+  {
+    initialRouteName: 'Home',
+    headerMode: "none",
+  }
+);
+
+HomeStack.navigationOptions = {
+  drawerLabel: 'Home',
+  drawerIcon: ({ tintColor }) => (
+    <Icon name='home'/>
+  ),
+};
+
+const Drawer = createDrawerNavigator(
+  {
+    HomeStack
+  }
+);
+export default HomeStack;
