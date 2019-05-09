ac69e1887e7b2bcb0d1c1203c036814ad1e74a4e
ioioio8888 <louislui95111@gmail.com>
2019-05-09 14:35:45 +0800

    Problem: Authstack doesnt has a SignUp and ForgotPassword Screen
    Solution: Add Signup and ForgotPassword Screen to Authstack

--- src/navigation/AuthStackNavigator.js
+++ src/navigation/AuthStackNavigator.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import { Platform } from 'react-native';
-import { createStackNavigator, createDrawerNavigator} from 'react-navigation';
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 
 import LoginScreen from '../screens/AuthStack/LoginScreen';
+import SignUpScreen from '../screens/AuthStack/SignUpScreen';
+import ForgotScreen from '../screens/AuthStack/ForgotScreen';
 import { Icon } from 'native-base';
 
 const AuthStack = createStackNavigator(
-  {
-    Login: {
-      screen: LoginScreen,
+    {
+        Login: {
+            screen: LoginScreen,
+        },
+        SignUp: {
+            screen: SignUpScreen,
+        },
+        Forgot: {
+            screen: ForgotScreen,
+        },
     },
-  },
-  {
-    initialRouteName: 'Login',
-    headerMode: "none",
-  }
+    {
+        initialRouteName: 'Login',
+        headerMode: "none",
+    }
 );
 
 
