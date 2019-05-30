030f1006884f612ecf71013f10f1e8aca39385d4
LouisLui <louislui95111@gmail.com>
2019-05-30 13:43:20 +0800

    Problem: Homepage is not showing server side data
    Solution: implement redux and meteor together to get data

--- src/redux/app-redux.js
+++ src/redux/app-redux.js
@@ -1,35 +1,63 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk';
-
+import { createStore, applyMiddleware } from "redux";
+import thunkMiddleware from "redux-thunk";
+import Meteor, { withTracker } from "react-native-meteor";
 //Initial State
 
 const InitialState = {
-    loggedIn: false,
-    user: null,
+  loggedIn: false,
+  user: null,
+  currency: {}
 };
 
 //Reducer
 
 const reducer = (state = InitialState, action) => {
-    switch(action.type){
-        case "setLoggedIn":
-            return { ...state, loggedIn: action.loggedIn, user: action.user}
-        default: return state;
-    }
+  switch (action.type) {
+    case "setLoggedIn":
+      return { ...state, loggedIn: action.loggedIn, user: action.user };
+    case "setCurrency":
+      return { ...state, currency: action.currency };
+    default:
+      return state;
+  }
 };
 
 //Store
 const store = createStore(reducer, applyMiddleware(thunkMiddleware));
 export { store };
 
-
 //action creator
 const setLoggedIn = (logging, user) => {
-    return {
-        type: "setLoggedIn",
-        loggedIn: logging,
-        user: user,
-    }
-}
+  return {
+    type: "setLoggedIn",
+    loggedIn: logging,
+    user: user
+  };
+};
 
-export {setLoggedIn};
+export { setLoggedIn };
+
+const setCurrency = currency => {
+  return {
+    type: "setCurrency",
+    currency: currency
+  };
+};
+
+export { setCurrency };
+
+const watchCurrencies = () => {
+  return function(dispatch) {
+    Meteor.subscribe("approvedcurrencies", {
+      onReady: function() {
+        Meteor.call("getTotalCurrencies", (err, data) => {
+            var r = Math.floor(Math.random() * data);
+            var currency = Meteor.collection("currencies").findOne({}, { skip: r });
+            dispatch(setCurrency(currency));
+          });
+      }
+    });
+  };
+};
+
+export { watchCurrencies };
